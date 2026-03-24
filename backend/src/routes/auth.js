1b54e1fd27866e69ec14d550644f5377e6b30f64
rohith1915 <rohithkr1912@gmail.com>
2026-03-24 23:34:15 +0530

    Delete backend directory

--- backend/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, getMe } = require('../controllers/authController');
-const auth = require('../middleware/auth');
-
-router.post('/login', login);
-router.get('/me', auth, getMe);
-
-module.exports = router;
